ecff50f39a0fe8b31be0dad57a3471924b934078
Ronald <ronald.jc@hotmail.com>
2021-01-08 13:04:53 -0300

    Foi adicionado a ediçao das postagens

--- Routes/admin.js
+++ Routes/admin.js
@@ -157,4 +157,48 @@ router.post('/postagem/nova', (req, res) => {
     }
 })
 
+router.get("/postagens/edit/:id", (req, res) => {
+
+    Postagem.findOne({ _id: req.params.id }).lean().then((postagem) => {
+
+        Categoria.find().lean().then((categorias) => {
+
+            res.render("admin/editpostagens", {categorias: categorias, postagem:postagem})
+
+        }).catch((error) => {
+            req.flash("error_msg", "Houve um erro ao listar as categorias")
+            res.redirect("/admin/postagens")
+        })
+
+    }).catch((error) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulário de edição")
+        res.redirect("/admin/postagens")
+    })
+})
+
+router.post("/postagem/edit", (req, res) => {
+    
+    Postagem.findOne({ _id:req.body.id}).then((postagem) => {
+
+        postagem.titulo = req.body.titulo
+        postagem.slug = req.body.slug
+        postagem.descricao = req.body.descricao
+        postagem.conteudo = req.body.conteudo
+        postagem.categoria = req.body.categoria
+
+        postagem.save().then(() => {
+            req.flash("success_msg", "As informações foram atualizadas com sucesso")
+            res.redirect("/admin/postagens")
+        }).catch((error) => {
+            req.flash("error_msg", "Houve um erro ao salvar as informações")
+            res.redirect("/admin/postagens")
+        })
+
+    }).catch((error) => {
+        req.flash("error_msg", "Houve um erro ao atualizar as informações")
+        res.redirect("/admin/postagens")
+    })
+
+})
+
 module.exports = router
