46ccf43ab7973ace75eab1d6a951b11f1b740712
Ronald <ronald.jc@hotmail.com>
2021-01-14 01:05:03 -0300

    Foi feita a exibicao de categorias e deletar

--- Routes/admin.js
+++ Routes/admin.js
@@ -8,6 +8,8 @@ require('../Models/Postagem')
 const Postagem = mongoose.model('postangens')
 const Categoria = mongoose.model('categorias')
 
+
+
 router.get('/', (req, res) => {
     res.render("admin/index")
 })
@@ -199,6 +201,16 @@ router.post("/postagem/edit", (req, res) => {
         res.redirect("/admin/postagens")
     })
 
+})
+
+router.get("/postagens/deletar/:id", (req, res) => {
+    Postagem.remove({_id: req.params.id}).then(() => {
+        req.flash("success_msg", "Postagem deletada com sucesso!")
+        res.redirect("/admin/postagens")
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao deletar a postagem!")
+        res.redirect("/admin/postagens")
+    })
 })
 
 module.exports = router
--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const path = require('path');
 const mongoose =  require('mongoose');
 const session = require('express-session')
 const flash = require('connect-flash')
+require('./Models/Postagem')
+require('./Models/Categoria')
 
+const Categoria = mongoose.model('categorias')
+const Postagem = mongoose.model('postangens')
 
 const app = express()
 
@@ -43,6 +47,68 @@ const app = express()
     app.use(express.static('public'));
 
 //Rotas 
+app.get('/', (req, res) => {
+    Postagem.find().populate("categoria").lean().sort({data: 'desc'}).limit(2).then((postagens) => {
+        res.render("index", {postagens: postagens})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect('/404')
+    })
+    
+})
+
+app.get('/postagem/:slug', (req, res) => {
+    Postagem.findOne({slug: req.params.slug}).lean().then((postagem) =>{
+        if(postagem){
+            res.render('postagem/index',{postagem:postagem})
+        }else{
+            req.flash("error_msg", "Esta postagem não existe ou não foi encontrada!")
+            res.redirect("/")
+        }
+    }).catch(err => {
+        req.flash("error_msg", "Houve um erro interno ")
+        res.redirect('/')
+    })
+})
+
+app.get('/categorias', (req, res) => {
+    Categoria.find().lean().then(categorias => {
+        res.render("categoria/index", {categorias: categorias})
+    }).catch(err => {
+        req.flash("error_msg", "Houve um error o encontrar as categorias!")
+        res.redirect("/")
+    })
+})
+
+app.get('/categorias/:slug', (req, res) => {
+    Categoria.findOne({ slug: req.params.slug }).lean().then((categoria) => {
+
+        if(categoria){
+            Postagem.find({categoria:categoria._id}).lean().then(postagens => {
+
+                res.render('categoria/postagens', {postagens: postagens, categoria: categoria})
+
+            }).catch(err => {
+                req.flash("error_msg", "Houve um erro ao listar posts")
+                res.redirect('/')
+            })
+
+        }else {
+            req.flash('error_msg', 'Esta categoria não existe ou não foi possível encontrar!')
+            res.redirect('/')
+        }
+
+    }).catch(err => {
+        req.flash("error_msg", "Houve um erro ao tentar encontrar essa categoria " )
+        res.redirect('/')
+    })
+})
+
+app.get('/404', (req, res) => {
+    res.send('Error: 404')
+})
+
+
 app.use('/admin', admin)
 
 const PORT = 3333;
